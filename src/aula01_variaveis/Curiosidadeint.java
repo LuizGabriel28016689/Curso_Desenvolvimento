1bf435bfa1fe4fe8aa6f95a95127789ac98e54e5
LUIZ GABRIEL DE OLIVEIRA NAZARIO <luiz_go_nazario@estudante.sesisenai.org.br>
2024-03-28 20:41:04 -0300

    Terceiro commit aula clayton

--- src/aula01_variaveis/Curiosidadeint.java
+++ src/aula01_variaveis/Curiosidadeint.java
@@ -2,12 +2,17 @@ package aula01_variaveis;
 
 public class Curiosidadeint {
     public static void main(String[] args) {
-        
         int var1 = 2147483647;
-        int var2 = 1;
+        int var2 = 2;
+        long var3 = 2147483647L;
+        long var4 = 2L;
 
-        System.out.println(var1);
-        System.out.println(var2);
-        System.out.println(var1 + var2);
+        System.out.println("Var 1: " + var1);
+        System.out.println("Var 2: " + var2);
+        System.out.println("Var 1 + Var 2: " + (var1 + var2));
+
+        System.out.println("Var 3: " + var3);
+        System.out.println("Var 4: " + var4);
+        System.out.println("Var 3 + Var 4: " + (var3 + var4));
     }
 }
