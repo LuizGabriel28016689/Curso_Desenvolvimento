01c78f8163e1cb8e1fbaf07c9a75d48f34a9f490
LUIZ GABRIEL DE OLIVEIRA NAZARIO <luiz_go_nazario@estudante.sesisenai.org.br>
2024-03-28 20:36:47 -0300

    Segundo commit aula clayton

--- src/aula01_variaveis/Curiosidadeint.java
+++ src/aula01_variaveis/Curiosidadeint.java
@@ -2,7 +2,12 @@ package aula01_variaveis;
 
 public class Curiosidadeint {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        
+        int var1 = 2147483647;
+        int var2 = 1;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var1 + var2);
     }
-    
 }
--- src/aula01_variaveis/VariaveisInteiras.java
+++ src/aula01_variaveis/VariaveisInteiras.java
@@ -1,18 +1,37 @@
 package aula01_variaveis;
 
 public class VariaveisInteiras {
-
     public static void main(String[] args) {
-        /**Variaveis do tipo Inteira */
-        byte idade1; //Declaração da variavel idade1: 20
+        /**Variaveis do tipo Inteira*/
+        byte idade1; //Declaração da variavel
         idade1 = 20; //Inicialização da variavel
-        short idade2 = 21; //Declaração e inicializaçãoi da variavel idade2: 21
+        short idade2 = 21; //Declaração e inicialização da variavel
         int idade3 = 23;
         long idade4 = 24;
-        System.out.println("Valor da variavel idade01:" + idade1);
-        System.out.println("Valor da variavel idade02:" + idade2);
-        System.out.println("Valor da variavel idade03:" + idade3);
-        System.out.println("Valor da variavel idade04:" + idade4);
-        System.out.println("Caralho isso é Java");
+
+        System.out.println("Valor da variavel idade01: " + idade1);
+        System.out.println("Valor da variavel idade02: " + idade2);
+        System.out.println("Valor da variavel idade03: " + idade3);
+        System.out.println("Valor da variavel idade04: " + idade4);
+
+        System.out.println("\n**** Exemplo 1 ****");
+        int numero = 10;
+        System.out.println(numero);
+
+        System.out.println("\n**** Exemplo 2 ****");
+        int soma = numero + 5;
+        System.out.println(soma);
+
+        System.out.println("\n**** Exemplo 3 ****");
+        long populacaoMundia = 7_900_000_00L;
+        System.out.println(populacaoMundia);
+
+        System.out.println("\n**** Exemplo 4 ****");
+        byte idadeMaxima = 127;
+        System.out.println(idadeMaxima);
+
+        System.out.println("\n**** Exemplo 5 ****");
+        short distanica= 10000;
+        System.out.println(distanica);
     }
 }
